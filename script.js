9ffb74c28569a09cf8b5e995342a8c5af663c117
Sohanur-01 <213902106@student.green.edu.bd>
2023-10-01 19:32:09 -0700

    js first module

--- script.js
+++ script.js
@@ -1,8 +1,47 @@
 //1-> Variable
-var a;
-let bananaPrice = 10;
-console.log(bananaPrice);
+// var a;
+// let bananaPrice = 10;
+// console.log(bananaPrice);
 
 //Data Types
-var name="Sohanur";
-console.log(typeof name);
+//String->
+// var name="Sohanur";
+// console.log(typeof name);
+// var name=`String name`;
+// console.log(name.toLowerCase());
+// console.log(name.toUpperCase());
+// console.log(name.indexOf('name'));
+// console.log(name.split(" "));
+// var n1=5;
+// var n2="15.5";
+// n2=parseFloat(n2);
+// console.log("Sum of two number: ",(n1+n2));
+
+//Mathematical Operations->
+// var n1=-234;
+// console.log(Math.abs(n1));
+
+// var n2=4.5345;
+// console.log(Math.round(n2));
+
+
+// var n2=4.5345;
+// console.log(Math.floor(n2));
+
+
+// var n2=4.5345;
+// console.log(Math.ceil(n2));
+
+
+//Conditions->
+// var n=prompt();
+// if(n>9 && n<15){
+//     document.write("Condition false");
+
+// }
+
+// else{
+//     document.write("Condition false");
+// }
+var date=new Date();
+console.log(date);
